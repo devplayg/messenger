3ace77a7588f45fc2461de35d8d17d375a6c7ce8
devplayg <devplayg@gmail.com>
2020-05-06 10:33:33 +0900

    added structure

--- entity/member.go
+++ entity/member.go
@@ -1,1 +1,29 @@
 package entity
+
+import "time"
+
+type Member struct {
+	MemberId int64
+	Name     string
+	Devices  []*Device
+}
+
+type Message struct {
+	Version   int
+	MessageId int64
+	Image     []byte
+	Text      string
+	From      int64 // Member
+	To        int64 // Device
+	Date      time.Time
+}
+
+type ChatRoom struct {
+	RoomId  int64
+	Members []*Member
+}
+
+type Device struct {
+	DeviceId int64
+	DevType  int
+}
